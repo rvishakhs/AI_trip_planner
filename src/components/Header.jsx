a670203dec05a2d6fdb9aebf5761e107b2df369a
rvishakhs <rvishakhs@gmail.com>
2025-05-17 11:23:17 +0100

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='p-2 shadow-sm flex justify-between items-center px-5'>
+      <img src='/logo.svg' className='w-44 h-18' />
+      <div>
+        <Button>Sign In</Button>
+      </div>
+    </div>
   )
 }
 
